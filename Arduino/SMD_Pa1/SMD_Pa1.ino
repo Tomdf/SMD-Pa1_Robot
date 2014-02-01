6db4e37cdf44b1ea8c6ff0b57ce325eb03c68336
Tomdf <tomdflock@gmail.com>
2014-01-31 21:56:10 -0700

    Initial Commit
    messin with some duino code

---
+++ Arduino/SMD_Pa1/SMD_Pa1.ino
@@ -0,0 +1,149 @@
+/* SparkFun WS2812 Breakout Board Example
+  SparkFun Electronics
+  date: July 25, 2013
+  license: GNU GENERAL PUBLIC LICENSE
+  
+  Requires the Adafruit NeoPixel library. It's awesome, go get it.
+  https://github.com/adafruit/Adafruit_NeoPixel
+  
+  This simple example code runs three sets of animations on a group of WS2812
+  breakout boards. The more boards you link up, the better these animations
+  will look. 
+  
+  For help linking WS2812 breakouts, checkout our hookup guide:
+  https://learn.sparkfun.com/tutorials/ws2812-breakout-hookup-guide
+  
+  Before uploading the code, make sure you adjust the two defines at the
+  top of this sketch: PIN and LED_COUNT. Pin should be the Arduino pin
+  you've got connected to the first pixel's DIN pin. By default it's
+  set to Arduino pin 4. LED_COUNT should be the number of breakout boards
+  you have linked up.
+*/
+#include <Servo.h> 
+
+#include <Adafruit_NeoPixel.h>
+#include "WS2812_Definitions.h"
+
+#define PIN 10
+#define LED_COUNT 1
+
+// Create an instance of the Adafruit_NeoPixel class called "leds".
+// That'll be what we refer to from here on...
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
+
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position
+const int pingPin = 7;
+void setup()
+{
+  Serial.begin(9600);
+  myservo.attach(9);
+  leds.begin();  // Call this to start up the LED strip.
+  clearLEDs();   // This function, defined below, turns all LEDs off...
+  setColor(PURPLE,1);
+}
+
+void loop()
+{
+  for(pos = 0; pos < 160; pos += 5)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+  delay(200);
+  Serial.print("PostionRight = ");
+  ping();
+  Serial.println();
+  
+  //delay(3000);
+  
+  for(pos = 160; pos>=1; pos-=5)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  }
+  delay(200);
+  Serial.print("PostionLeft = ");
+  ping();
+  Serial.println();
+  
+  delay(3000);  
+}
+
+void setColor(unsigned long color, byte brightness)
+{
+  byte red = (color & 0xFF0000) >> 16;
+  byte green = (color & 0x00FF00) >> 8;
+  byte blue = (color & 0x0000FF);
+  
+  for (int i=0; i<=LED_COUNT-1; i++)
+  {
+    leds.setPixelColor(i, red/brightness, green/brightness, blue/brightness);
+  }
+  leds.show();  // Turn the LEDs on
+}
+
+// Sets all LEDs to off, but DOES NOT update the display;
+// call leds.show() to actually turn them off after this.
+void clearLEDs()
+{
+  for (int i=0; i<LED_COUNT; i++)
+  {
+    leds.setPixelColor(i, 0);
+    leds.show();
+  }
+}
+
+void ping()
+{
+  // establish variables for duration of the ping,
+  // and the distance result in inches and centimeters:
+  long duration, inches, cm;
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+  // The same pin is used to read the signal from the PING))): a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+ 
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+ 
+  delay(100);
+}
+
+long microsecondsToInches(long microseconds)
+{
+  // According to Parallax's datasheet for the PING))), there are
+  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+  // second).  This gives the distance travelled by the ping, outbound
+  // and return, so we divide by 2 to get the distance of the obstacle.
+  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
