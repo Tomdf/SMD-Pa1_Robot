a15676f7b1c221f0346f251bfd877135e5ad865d
Tomdf <tomdflock@gmail.com>
2014-02-04 20:47:40 -0700

    Arduino code and hookup guide

--- Arduino/SMD_Pa1/SMD_Pa1.ino
+++ Arduino/SMD_Pa1/SMD_Pa1.ino
@@ -1,74 +1,104 @@
-/* SparkFun WS2812 Breakout Board Example
-  SparkFun Electronics
-  date: July 25, 2013
-  license: GNU GENERAL PUBLIC LICENSE
-  
-  Requires the Adafruit NeoPixel library. It's awesome, go get it.
-  https://github.com/adafruit/Adafruit_NeoPixel
-  
-  This simple example code runs three sets of animations on a group of WS2812
-  breakout boards. The more boards you link up, the better these animations
-  will look. 
-  
-  For help linking WS2812 breakouts, checkout our hookup guide:
-  https://learn.sparkfun.com/tutorials/ws2812-breakout-hookup-guide
-  
-  Before uploading the code, make sure you adjust the two defines at the
-  top of this sketch: PIN and LED_COUNT. Pin should be the Arduino pin
-  you've got connected to the first pixel's DIN pin. By default it's
-  set to Arduino pin 4. LED_COUNT should be the number of breakout boards
-  you have linked up.
-*/
-#include <Servo.h> 
+/* Code for the SMD-PA1 robot
+ Tom Flock
+ 2-4-14
+ 
+ Requires the Adafruit NeoPixel library. It's awesome, go get it.
+ https://github.com/adafruit/Adafruit_NeoPixel
+ 
+ This simple example code runs three sets of animations on a group of WS2812
+ breakout boards. The more boards you link up, the better these animations
+ will look. 
+ 
+ For help linking WS2812 breakouts, checkout our hookup guide:
+ https://learn.sparkfun.com/tutorials/ws2812-breakout-hookup-guide
+ 
+ Before uploading the code, make sure you adjust the two defines at the
+ top of this sketch: PIN and LED_COUNT. Pin should be the Arduino pin
+ you've got connected to the first pixel's DIN pin. By default it's
+ set to Arduino pin 4. LED_COUNT should be the number of breakout boards
+ you have linked up.
+ */
+#include <Servo.h>    // include servo library
+#include "pitches.h"  // include pitches h file?
 
 #include <Adafruit_NeoPixel.h>
 #include "WS2812_Definitions.h"
 
-#define PIN 10
-#define LED_COUNT 1
+// Definitions for use with the NeoPixel library
+#define PIN 7    // WS2812 pin
+#define LED_COUNT 1    // the number of WS2812s connected
 
 // Create an instance of the Adafruit_NeoPixel class called "leds".
 // That'll be what we refer to from here on...
 Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
 
 Servo myservo;  // create servo object to control a servo 
-                // a maximum of eight servo objects can be created 
- 
+
+const int pingPin = 4;    // connection to ultrasonic sensor
+int headLED = 9;
 int pos = 0;    // variable to store the servo position
-const int pingPin = 7;
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_E3, NOTE_G3, 0, NOTE_F3, NOTE_A3, NOTE_A4, NOTE_B4};
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  8, 10, 6, 8,10,8,8,10 };
+
 void setup()
 {
   Serial.begin(9600);
-  myservo.attach(9);
+  myservo.attach(5);
+  pinMode(headLED, OUTPUT);
   leds.begin();  // Call this to start up the LED strip.
   clearLEDs();   // This function, defined below, turns all LEDs off...
   setColor(PURPLE,1);
+
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second 
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(10, melody[thisNote],noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(10);
+  }
 }
 
-void loop()
-{
-  for(pos = 0; pos < 160; pos += 5)  // goes from 0 degrees to 180 degrees 
+void loop(){
+  digitalWrite(headLED, HIGH);
+  for(pos = 20; pos < 160; pos += 5)  // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    myservo.write(pos);    // tell servo to go to position in variable 'pos' 
     delay(15);                       // waits 15ms for the servo to reach the position 
   } 
   delay(200);
+
   Serial.print("PostionRight = ");
   ping();
   Serial.println();
-  
-  //delay(3000);
-  
-  for(pos = 160; pos>=1; pos-=5)     // goes from 180 degrees to 0 degrees 
+
+  delay(3000);
+
+  digitalWrite(headLED, LOW);
+  for(pos = 160; pos>=25; pos-=5)     // goes from 180 degrees to 0 degrees 
   {                                
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(15);                       // waits 15ms for the servo to reach the position 
   }
   delay(200);
+
   Serial.print("PostionLeft = ");
   ping();
   Serial.println();
-  
+
   delay(3000);  
 }
 
@@ -77,7 +107,7 @@ void setColor(unsigned long color, byte brightness)
   byte red = (color & 0xFF0000) >> 16;
   byte green = (color & 0x00FF00) >> 8;
   byte blue = (color & 0x0000FF);
-  
+
   for (int i=0; i<=LED_COUNT-1; i++)
   {
     leds.setPixelColor(i, red/brightness, green/brightness, blue/brightness);
@@ -120,13 +150,13 @@ void ping()
   // convert the time into a distance
   inches = microsecondsToInches(duration);
   cm = microsecondsToCentimeters(duration);
- 
+
   Serial.print(inches);
   Serial.print("in, ");
   Serial.print(cm);
   Serial.print("cm");
   Serial.println();
- 
+
   delay(100);
 }
 
@@ -147,3 +177,7 @@ long microsecondsToCentimeters(long microseconds)
   // object we take half of the distance travelled.
   return microseconds / 29 / 2;
 }
+
+
+
+
